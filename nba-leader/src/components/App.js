de3ae499a277efced4227f536753f8c56e17097f
BookmDan <danoh329@gmail.com>
2023-10-10 20:54:51 -0700

    changed from currentPage to React Router

--- nba-leader/src/components/App.js
+++ nba-leader/src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect  }  from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PlayerList from './PlayerList';
 import NavBar from './NavBar';
 import Header from './Header';
@@ -8,23 +9,37 @@ import TopPlayers from './TopPlayers';
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   // const [players, setPlayers] = useState([]);
-  const [currentPage, setCurrentPage] = useState(null);
+  // const [currentPage, setCurrentPage] = useState(null);
 
   function handleDarkModeClick() {
     setIsDarkMode((prevIsDarkMode) => !prevIsDarkMode);
   }
 
-  const handleChangePage = (page) => {
-    setCurrentPage(page);
-  };
+  // const handleChangePage = (page) => {
+  //   setCurrentPage(page);
+  // };
+
+  // <div>
+  // <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+  // <NavBar onChangePage={handleChangePage} />
+  // {currentPage && <TopPlayers category={currentPage} />}
+  // <PlayerList />
+  
+  // </div>
+  // isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} 
   return (
-    <div>
-      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <NavBar onChangePage={handleChangePage} />
-      {currentPage && <TopPlayers category={currentPage} />}
-      <PlayerList />
-      
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <NavBar />
+        <Switch>
+          <Route path="/offensive" render={() => <TopPlayers category="offensive" />} />
+          <Route path="/defensive" render={() => <TopPlayers category="defensive" />} />
+          <Route path="/assists" render={() => <TopPlayers category="assists" />} />
+          <Route path="/" component={PlayerList} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- nba-leader/src/components/NavBar.js
+++ nba-leader/src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function NavBar({ onChangePage }) {
+function NavBar() {
   return (
     <nav>
-      <button onClick={() => onChangePage("offensive")}>Top Offensive Players</button>
-      <button onClick={() => onChangePage("defensive")}>Top Defensive Players</button>
-      <button onClick={() => onChangePage("assists")}>Top Assists Players</button>
+      <Link to="/offensive">Top Offensive Players</Link>
+      <Link to="/defensive">Top Defensive Players</Link>
+      <Link to="/assists">Top Assists Players</Link>
     </nav>
   );
 }
