f7c4e1459e7592b6d1b8c8597e0913f3bc58b948
BookmDan <danoh329@gmail.com>
2023-10-10 20:42:57 -0700

    css stuff, frustrating

--- nba-leader/src/components/TopPlayers.js
+++ nba-leader/src/components/TopPlayers.js
@@ -3,56 +3,56 @@ import PlayerItem from './PlayerItem';
 
 const TopPlayers = ({category}) => {
   const [topPlayers, setTopPlayers] = useState([]);
-  const [tatumPlayer, setTatumPlayer] = useState(null); 
-
-  useEffect(() => {
-    const fetchPlayers = async () => {
-      try {
-        const response = await fetch('../../db.json');
-        const data = await response.json();
-        const jaysonTatum = data.players.find(player => player.player_name === "Jayson Tatum");
-        if (jaysonTatum) {
-          setTatumPlayer(jaysonTatum);
-        } else {
-          console.error('Jayson Tatum not found.');
-        }
-      } catch (error) {
-        console.error('Error fetching player data:', error);
-      }
-    };
-
-    fetchPlayers();
-  }, []);
+  // const [tatumPlayer, setTatumPlayer] = useState(null); 
 
   // useEffect(() => {
-  //   fetch('../../db.json')
-  //     .then(response => response.json())
-      // .then(data => {
-      //   let sortedPlayers;
-      //   if (category === 'offensive') {
-      //     sortedPlayers = data.players.sort((a, b) => (b.PTS / b.minutes_played) - (a.PTS / a.minutes_played));
-      //   } else if (category === 'defensive') {
-      //     sortedPlayers = data.players.sort((a, b) => (b.BLK / b.minutes_played) - (a.BLK / a.minutes_played));
-      //   } else if (category === 'assists') {
-      //     sortedPlayers = data.players.sort((a, b) => b.AST - a.AST);
-      //   }
+  //   const fetchPlayers = async () => {
+  //     try {
+  //       const response = await fetch('../../db.json');
+  //       const data = await response.json();
+  //       const jaysonTatum = data.players.find(player => player.player_name === "Jayson Tatum");
+  //       if (jaysonTatum) {
+  //         setTatumPlayer(jaysonTatum);
+  //       } else {
+  //         console.error('Jayson Tatum not found.');
+  //       }
+  //     } catch (error) {
+  //       console.error('Error fetching player data:', error);
+  //     }
+  //   };
 
-        // const sortedPlayers = data.players.sort((a, b) => (b.PTS / b.minutes_played) - (a.PTS / a.minutes_played));
-  //       setTopPlayers(sortedPlayers.slice(0, 3));  // Top 3 players by offensive metric
-  //     })
-  //     .catch(error => console.error('Error fetching player data:', error));
-  // }, [category]);
+  //   fetchPlayers();
+  // }, []);
 
+  useEffect(() => {
+    fetch('../../db.json')
+      .then(response => response.json())
+      .then(data => {
+        // Sort based on the relevant metric for the category
+        let sortedPlayers;
+        if (category === 'offensive') {
+          sortedPlayers = data.players.sort((a, b) => (b.PTS / b.minutes_played) - (a.PTS / a.minutes_played));
+        } else if (category === 'defensive') {
+          sortedPlayers = data.players.sort((a, b) => (b.BLK / b.minutes_played) - (a.BLK / a.minutes_played));
+        } else if (category === 'assists') {
+          sortedPlayers = data.players.sort((a, b) => b.AST - a.AST);
+        }
+        
+        setTopPlayers(sortedPlayers.slice(0, 3));  // Top 3 players based on the metric
+      })
+      .catch(error => console.error('Error fetching player data:', error));
+  }, [category]);
+
+  // <h1>Jayson Tatum - Top Offensive Player</h1>
+  // {tatumPlayer && <PlayerItem player={tatumPlayer} />}
   return (
     <div>
-      {/* <h2>Top 3 {category.charAt(0).toUpperCase() + category.slice(1)} Players</h2>
+      <h2>Top 3 {category.charAt(0).toUpperCase() + category.slice(1)} Players</h2>
       <ul>
         {topPlayers.map(player => (
           <PlayerItem key={player.id} player={player} category={category} />
         ))}
-      </ul> */}
-      <h1>Jayson Tatum - Top Offensive Player</h1>
-      {tatumPlayer && <PlayerItem player={tatumPlayer} />}
+      </ul>
     </div>
   );
 };
