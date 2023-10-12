daa573cc43226e7c5fb16a8416720a43b9c2d848
BookmDan <danoh329@gmail.com>
2023-10-12 12:07:01 -0700

    made changes to css

--- nba-leader/src/components/Header.js
+++ nba-leader/src/components/Header.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-function Header({ isDarkMode, onDarkModeClick }) {
+function Header() {
   return (
     <header>
       <h2>NBA 2023 Top Charts</h2>
-      {/* <button onClick={onDarkModeClick}>
-        {isDarkMode ? 'Dark' : 'Light'} Mode
-      </button> */}
     </header>
   );
 }
--- nba-leader/src/components/PlayerItem.js
+++ nba-leader/src/components/PlayerItem.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const PlayerItem = ({ player, category }) => {
+  const offensiveRating = ((player.PTS / player.minutes_played) * 100).toFixed(2);
+  const defensiveRating = (player.BLK * 0.6 + player.STL * 0.4).toFixed(2);
+
   return (
     <div className="card">
-      <h2>{player.player_name}</h2>
-      <img src={player.image} alt={player.player_name} className="player-avatar" />
-      <p>Points: {player.PTS}</p>
-      <p>Offensive Rating: {((player.PTS / player.minutes_played) * 100).toFixed(2)}</p>
-      <p>Defensive Rating: {(player.BLK*.6 + player.STL*.4).toFixed(2)}</p>
-      <p>Assists: {player.AST}</p>
+      <div className="card-content">
+        <h2>{player.player_name}</h2>
+        <img src={player.image} alt={player.player_name} className="player-avatar" />
+        <p>Points: {player.PTS}</p>
+        <p>Offensive Rating: {offensiveRating}</p>
+        <p>Defensive Rating: {defensiveRating}</p>
+        <p>Assists: {player.AST}</p>
+      </div>
 
-      {/* Display other offensive player information as needed */}
-      {category === 'offensive' && <p>Offensive Rating: {(player.PTS / player.minutes_played) * 100}</p>}
-      {category === 'defensive' && <p>Defensive Rating: {(player.BLK*.6 + player.STL*.4)}</p>}
-      {category === 'assists' && <p>Assists: {player.AST}</p>}
     </div>
   );
 };
